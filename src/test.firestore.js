b6e5827a81eefbc9a4f991aedfb8d5e54160f3f2
Alexis0215 <49080925+Alexis0215@users.noreply.github.com>
2020-12-01 13:58:08 -0800

    user info

--- src/test.firestore.js
+++ src/test.firestore.js
@@ -5,3 +5,34 @@ import firebase from 'firebase';
 const app = firebaseApp;
 const db = firebase.firestore(app);
 
+
+async function FirebaseData(userID){
+    // now async 
+    let username = null;
+    var docRef = db.collection("users").doc(userID);
+
+    let user = await docRef.get().then(function(doc) { // .get returns a promise -> give it a function that doesnt execute right awat it doesnt execute right away
+        // awaiting the promise , .then() chaining a promising onto it, executes our function, 
+        if (doc.exists) {
+            console.log("Document exist");
+            console.log(doc.data());
+            console.log(doc.data().name);
+            username = doc.data().name;
+           return username;
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+return user;    
+    
+}
+
+export default FirebaseData;
+
+
+
+
