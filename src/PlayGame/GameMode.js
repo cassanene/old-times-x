5cff15883d93bbb587d1bbe99967f07bd2cfde3c
Cassey Anene <casseyanene@gmail.com>
2020-11-30 22:23:52 -0600

    added select for game mode

--- src/PlayGame/GameMode.js
+++ src/PlayGame/GameMode.js
@@ -3,25 +3,54 @@ import NavigationBar from '../NavigationBar';
 import {Button} from "baseui/button";
 import {Card} from "baseui/card";
 import Routes from "../Route";
+import {Select, TYPE} from 'baseui/select';
+import { Notification, KIND} from "baseui/notification";
 
 export default function Game (){
+    const [value, setValue] = React.useState([""]);
+    const [error, setError] = React.useState(false);
 
-    function handleClick (e){
-        window.location = e.target.id;
+    function handleClick (){
+        if (value == ""){
+            setError(true);
+        }
+        else{
+            window.location = value[0].route;
         return(
             <Routes />
         )
+        }
+        // setError(false);
+        
     }
     return(
-    <div>
-        <NavigationBar />
-        <Card>
-            <Button id="/singleplayer/play/hard" onClick={(e) => handleClick(e)} >Single Player Game: Hard</Button>
-        </Card>
-        <Card>
-            <Button id="/singleplayer/play/easy" onClick={(e) => handleClick(e)} >Single Player Game: Easy</Button>
-        </Card>
-     </div>
+<div> 
+    <NavigationBar />
+    {
+        error && (
+            <Notification kind={KIND.negative}>
+             {() => "Select a game to continue."}
+           </Notification>
+        )
+    }
+    <Card> 
+        <Select
+        options={[
+        {id: 'Easy Mode', route: "/singleplayer/play/easy"},
+        {id: 'Hard Mode', route: "/singleplayer/play/hard"},
+        ]}
+        labelKey="id"
+        valueKey="route"
+        placeholder="Choose a game mode"
+        maxDropdownHeight="300px"
+        onChange={({value}) => setValue(value)}
+        value={value}
+        />
+    </Card>
+    <Button onClick={handleClick}>
+        Play {value[0].id}
+    </Button>
+</div>
     );    
 }
 
